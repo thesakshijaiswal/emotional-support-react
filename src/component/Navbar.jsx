dc8ca926807e8855c7aa235dae6bd526ad5c6e69
Sakshi Jaiswal <thesakshijaiswal@outlook.com>
2024-10-14 19:55:33 +0530

    feat: create navbar

---
+++ src/component/Navbar.jsx
@@ -0,0 +1,25 @@
+import menubar from "../assets/navbar/menubar.png";
+import logo from "../assets/navbar/logo.png";
+import resources from "../assets/navbar/resources.png";
+import navigation from "../assets/navbar/navigation.png";
+import exit from "../assets/navbar/exit.png";
+const Navbar = () => {
+  return (
+    <nav className="bg-[#a993c5] h-24 flex justify-between">
+      <div className="leftNav flex">
+        <img src={menubar} alt="" className="h-24" />
+        <img src={logo} alt="" className="h-24 p-5 bg-white" />
+      </div>
+      <div className="head font-semibold text-3xl flex items-center -ml-96">
+        Introduction to the Emotional Support Module
+      </div>
+      <div className="rightNav flex">
+        <img src={resources} alt="" />
+        <img src={navigation} alt="" className="h-24" />
+        <img src={exit} alt="" className="h-24" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
